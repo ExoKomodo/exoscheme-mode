3666bc9fd341d30ac8a8f5523d774ee973b90964
James Orson <jamesaorson@gmail.com>
2024-10-01 09:42:43 -0700

    Fix symbol and type matching. First starting version is complete

--- exoscheme-mode.el
+++ exoscheme-mode.el
@@ -11,18 +11,26 @@
   '("char" "int" "long" "short" "void" "bool" "float" "double" "size_t"))
 
 (defun exoscheme-builtins ()
-  '("define" "define*" "define-module" "use-modules" "use-module"))
+  '("define" "define*" "define-module" "use-modules" "use-module" "lambda" "lambda*"))
 
 (defun exoscheme-keywords ()
-	'("let" "cond" "if" "else" "when" "nil"))
+	'("let" "let*" "cond" "if" "else" "when" "nil"))
+
+(defun exoscheme-specials ()
+	'("," "'" "`" ",@"))
+
+(defun exoscheme-constants ()
+	'("#t" "#f"))
 
 (defun exoscheme-font-lock-keywords ()
   `(
+	(";.*" . font-lock-comment-face)
     (,(regexp-opt (exoscheme-keywords) 'symbols) . font-lock-keyword-face)
     (,(regexp-opt (exoscheme-builtins) 'symbols) . font-lock-builtin-face)
     (,(regexp-opt (exoscheme-types) 'symbols) . font-lock-type-face)
-    ("#[tf]" . font-lock-constant-face)
-	(";.*" . font-lock-comment-face)
+    (,(regexp-opt (exoscheme-constants)) . font-lock-constant-face)
+    (,(regexp-opt (exoscheme-specials)) . font-lock-warning-face)
+    ("<[a-zA-Z-_][a-zA-Z0-9-_]*>" . font-lock-type-face)
 	("#:[a-zA-Z-_][a-zA-Z0-9-_]*" . font-lock-variable-name-face)
 	("(\\([a-zA-Z-_][a-zA-Z0-9-_/]*\\)" . (1 font-lock-function-name-face))))
 
